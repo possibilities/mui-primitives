e2b826a9b1688e87d8d3c91945998bf43d507543
Mike Bannister <notimpossiblemike@gmail.com>
2020-05-31 03:22:56 -0400

    Generate static types

---
+++ generate-static-types.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// See: https://github.com/possibilities/mui-primitives/pull/3#issue-425668524
+const getStaticTypes = require('playroom/lib/getStaticTypes')
+const generateStaticTypes = async () =>
+  console.info(JSON.stringify(await getStaticTypes({
+    typeScriptFiles: ['./components/*.tsx']
+  }), null, 2))
+generateStaticTypes()
